b91c73f04d3761fc769a75c8d209f66712464484
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-08-29 15:40:12 -0300

    feat: Adiciona rota bruxos com filter

---
+++ server.js
@@ -0,0 +1,54 @@
+// Importar pacotes/bibliotecas
+import express from "express";
+import dotenv from "dotenv";
+
+import dados from "./src/data/dados.js"
+const {bruxos} = dados;
+
+// Criar aplicação com Express e configurar para aceitar JSON
+const app = express();
+app.use(express.json());        
+
+// Carregar variáveis de ambiente e definir constante para porta do servidor
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+// Rota principal GET para "/"
+app.get("/", (req, res) => {
+    res.send("🚀 Servidor funcionando...");
+});
+
+
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+});
+
+
+// Get com filtros
+app.get('/bruxos', (req, res) => {
+    const { casa, ano, especialidade, nome } = req.query;
+    let resultado = bruxos;
+  
+    if (casa) {
+      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+    }
+  
+    if (ano) {
+      resultado = resultado.filter(b => b.ano == ano);
+    }
+  
+    if (especialidade) {
+      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+    }
+  
+    if (nome) {
+      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+  
+    res.status(200).json({
+      total: resultado.length,
+      data: resultado
+    });
+});
